139e2123d08865bbe7a7f4a32a74287044310e31
ScCarlosCT <carlos.cervantes0521@gmail.com>
2025-06-05 05:51:54 -0600

    chore: url modificado

--- interfaz/urls.py
+++ interfaz/urls.py
@@ -1,9 +1,9 @@
 from django.contrib import admin
 from django.urls import path
-from appi import views  
+from appi import views 
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),  # Ruta para la página principal
-    path('registrar/', views.registrar_cliente, name='registrarCliente'),
+    path('registrar/', views.registrarCliente, name='registrarCliente'),  # Nombre de la vista corregido
 ]
